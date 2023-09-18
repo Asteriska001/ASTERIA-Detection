d6cc2f4f95637dd67ce3dd1e1295f4f465cf0730
Asteriska8 <1758995741@qq.com>
2023-09-18 10:09:42 +0800

    add todo.

--- framework/models/Devign.py
+++ framework/models/Devign.py
@@ -67,6 +67,8 @@ class Devign(nn.Module):
         
         config_class, model_class, tokenizer_class = MODEL_CLASSES[tokenizer]
         
+        #todo: config,encoder,tokenizer = get_pretrained(args.config_name,args.model_name,args.tokenizer_name)
+
         config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path,
                                              )#cache_dir=args.cache_dir if args.cache_dir else None)
         self.config = config
